61fb57d90e134bc51330c68682a2c85e728391b1
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-05-09 10:28:11 +0530

    feat - movie overview

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -10,15 +10,12 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
 				<h3>{title}</h3>
 				<span>{vote_average}</span>
 			</div>
+			<div className="movie-overview">
+				<h2>{title}</h2>
+				<p>{overview}</p>
+			</div>
 		</div>
 	);
 };
 
 export default Movie;
-
-/*
-<div className="movie-overview">
-				<h2>{title}</h2>
-				<p>{overview}</p>
-			</div>
-*/
